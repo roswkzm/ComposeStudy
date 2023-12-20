328e2e408420d7b9b300bb2f453aa094a2d6ddc3
jylee <jylee@maxst.com>
2023-12-20 10:58:05 +0900

    Compose Box

--- app/src/main/java/com/example/composestudy/MainActivity.kt
+++ app/src/main/java/com/example/composestudy/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -22,6 +24,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,26 +36,27 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeStudyTheme {
-                Greeting("Android")
+                Greeting()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name : String) {
-    Surface(
-        modifier = Modifier.padding(5.dp),
-        shadowElevation = 10.dp,
-        border = BorderStroke(width = 2.dp, color = Color.Magenta),
-        shape = CircleShape,
-        color = MaterialTheme.colorScheme.error,
-        contentColor = Color.Black
-    ) {
-        Text(
-            text = "Hello $name!",
-            modifier = Modifier.padding(8.dp)
-        )
+fun Greeting() {
+    Box(){
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Cyan)
+            .align(Alignment.CenterStart)){
+            Text(text = "Hello Android")
+        }
+        Box(modifier = Modifier
+            .size(70.dp)
+            .background(Color.Yellow)
+            .align(Alignment.Center)){
+            Text(text = "Hello JetPack")
+        }
     }
 }
 
@@ -60,6 +64,6 @@ fun Greeting(name : String) {
 @Composable
 fun DefaultPreview() {
     ComposeStudyTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
