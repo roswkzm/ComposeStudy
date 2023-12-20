4630bc60ab931fbe676fd16368f39a66331b3daa
jylee <jylee@maxst.com>
2023-12-20 18:51:14 +0900

    Compose Scaffold

--- app/src/main/java/com/example/composestudy/MainActivity.kt
+++ app/src/main/java/com/example/composestudy/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.example.composestudy
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,7 +27,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composestudy.ui.theme.ComposeStudyTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,44 +44,63 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting() {
-    var checked1 by remember { mutableStateOf(false) }
-    var checked2 by remember { mutableStateOf(false) }
-
-    Column {
-        CheckBoxWithSlotApi(checked = checked1,
-            onCheckedChanged = {checked1 = !checked1}
-        ) {
-            Text(text = "텍스트 1", modifier = Modifier.align(Alignment.CenterVertically))
+    var checked by remember { mutableStateOf(false) }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Scaffold App")
+                },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Image(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "뒤로 가기"
+                        )
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+            }
         }
-        CheckBoxWithSlotApi(checked = checked2,
-            onCheckedChanged = {checked2 = !checked2}
         ) {
-            Text(text = "텍스트 1", modifier = Modifier.align(Alignment.CenterVertically))
+        Surface(modifier = Modifier
+            .padding(8.dp)
+            .fillMaxSize()) {
+            CheckBoxWithContent(
+                checked = checked,
+                toggleState = { checked = !checked }
+            ) {
+                Text(text = "컴포즈를 좋아합니다.", color = Color.Black)
+            }
         }
     }
 }
 
 @Composable
-fun CheckBoxWithSlotApi(
+fun CheckBoxWithContent(
     checked: Boolean,
-    onCheckedChanged : () -> Unit,
-    content: @Composable RowScope.() -> Unit
-) {
+    toggleState : () -> Unit,
+    content : @Composable RowScope.() -> Unit
+){
     Row(
         verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.clickable {
-            onCheckedChanged()
-        }
+        modifier = Modifier.clickable { toggleState() }
     ) {
         Checkbox(
             checked = checked,
-            onCheckedChange = { onCheckedChanged() }
+            onCheckedChange = {toggleState()}
         )
         content()
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
