73fba0f75c943e86895ce5a814ca1b1ba379a610
jylee <jylee@maxst.com>
2023-12-20 11:12:49 +0900

    Compose Row

--- app/src/main/java/com/example/composestudy/MainActivity.kt
+++ app/src/main/java/com/example/composestudy/MainActivity.kt
@@ -6,16 +6,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -27,6 +33,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composestudy.ui.theme.ComposeStudyTheme
@@ -44,19 +51,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting() {
-    Box(){
-        Box(modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Cyan)
-            .align(Alignment.CenterStart)){
-            Text(text = "Hello Android")
-        }
-        Box(modifier = Modifier
-            .size(70.dp)
-            .background(Color.Yellow)
-            .align(Alignment.Center)){
-            Text(text = "Hello JetPack")
-        }
+    Row(
+        modifier = Modifier.height(40.dp).width(300.dp),
+        verticalAlignment = Alignment.Bottom,
+        horizontalArrangement = Arrangement.SpaceEvenly
+        ){
+        Text(
+            text = "첫 번째!!",
+            textAlign = TextAlign.Center,
+            modifier = Modifier.align(Alignment.Top)
+                .weight(3f)
+                .background(Color.Magenta)
+        )
+
+        Icon(
+            imageVector = Icons.Filled.AccountBox,
+            contentDescription = "추가",
+            modifier = Modifier.weight(1f).background(Color.Cyan)
+        )
+
+        Text(
+            text = "세 번째!!",
+            modifier = Modifier.weight(3f).background(Color.Blue)
+        )
     }
 }
 
