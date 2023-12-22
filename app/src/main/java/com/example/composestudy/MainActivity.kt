a40fa6cbe892f89c4ddd6c1359c1e8f76df108e5
jylee <jylee@maxst.com>
2023-12-22 13:36:22 +0900

    ConstraintLayout Use Card

--- app/src/main/java/com/example/composestudy/MainActivity.kt
+++ app/src/main/java/com/example/composestudy/MainActivity.kt
@@ -3,26 +3,18 @@ package com.example.composestudy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ChainStyle
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
-import coil.compose.AsyncImage
 import com.example.composestudy.ui.theme.ComposeStudyTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,104 +23,40 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeStudyTheme {
                 Column {
-                    Greeting(cardData)
+                    Greeting()
                 }
             }
         }
     }
 
-    companion object{
-        val cardData = CardData(
-            imageUri = "https://raw.githubusercontent.com/roswkzm/ComposeStudy/32c8095f80aae234ed5190102e2091a9d18190e0/app/src/main/res/drawable/wall.jpg",
-            imageDescription = "캐년",
-            author = "Dalinaum",
-            description = "으아으아으아으아으아으아으ㅏ으아으아으아으ㅏ"
-        )
+    @Composable
+    fun Greeting() {
+        Canvas(modifier = Modifier.size(20.dp)){
+            drawLine(Color.Red, Offset(30f, 10f), Offset(50f, 40f))
+
+            drawCircle(Color.Yellow, 10f, Offset(15f, 40f))
+
+            drawRect(Color.Magenta, Offset(30f, 30f), Size(10f, 10f))
+
+            Icons.Filled.Send
+
+            drawLine(Color.Green, Offset(2.01f, 21.0f), Offset(23.0f, 12.0f))
+            drawLine(Color.Green, Offset(23.0f, 12.0f), Offset(2.01f, 3.0f))
+            drawLine(Color.Green, Offset(2.01f, 3.0f), Offset(2.0f, 10.0f))
+            drawLine(Color.Green, Offset(2.0f, 10.0f), Offset(17.0f, 12.0f))
+            drawLine(Color.Green, Offset(17.0f, 12.0f), Offset(2.0f, 14.0f))
+            drawLine(Color.Green, Offset(2.0f, 14.0f), Offset(2.01f, 21.0f))
+        }
     }
-}
 
-@Composable
-fun Greeting(cardData : CardData) {
-    Card(
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 8.dp
-        ),
-        modifier = Modifier.padding(4.dp)
-    ) {
-        ConstraintLayout(modifier = Modifier
-            .fillMaxWidth()) {
-            val (profileImage, author, description) = createRefs()
 
-            AsyncImage(
-                model = cardData.imageUri,
-                contentDescription = cardData.imageDescription,
-                contentScale = ContentScale.Crop,
-                placeholder = ColorPainter(Color.Black),
-                modifier = Modifier
-                    .size(40.dp)
-                    .clip(CircleShape)
-                    .constrainAs(profileImage) {
-                        centerVerticallyTo(parent)
-                        start.linkTo(parent.start, margin = 8.dp)
-                    }
-            )
-
-            Text(
-                text = cardData.author,
-                modifier = Modifier.constrainAs(author) {
-                    linkTo(
-                        profileImage.end,
-                        parent.end,
-                        startMargin = 8.dp,
-                        endMargin = 8.dp
-                    )
-                    width = Dimension.fillToConstraints
-                }
-            )
-
-            Text(
-                text = cardData.description,
-                modifier = Modifier.constrainAs(description){
-                    linkTo(
-                        profileImage.end,
-                        parent.end,
-                        startMargin = 8.dp,
-                        endMargin = 8.dp
-                    )
-                    width = Dimension.fillToConstraints
-                }
-            )
-
-            val chain = createVerticalChain(
-                author,
-                description,
-                chainStyle = ChainStyle.Packed
-            )
-
-            constrain(chain){
-                top.linkTo(parent.top, margin = 8.dp)
-                bottom.linkTo(parent.bottom, margin = 8.dp)
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        ComposeStudyTheme {
+            Column {
+                Greeting()
             }
         }
     }
 }
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeStudyTheme {
-        Column {
-            Greeting(MainActivity.cardData)
-            Greeting(MainActivity.cardData)
-            Greeting(MainActivity.cardData)
-            Greeting(MainActivity.cardData)
-        }
-    }
-}
-
-data class CardData(
-    val imageUri: String,
-    val imageDescription: String,
-    val author: String,
-    val description: String
-)
