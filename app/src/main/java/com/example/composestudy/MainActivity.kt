cd24c86cfbc33878a93e8468edd8f7d318c67235
jylee <jylee@maxst.com>
2023-12-22 14:18:07 +0900

    CustomDialog

--- app/src/main/java/com/example/composestudy/MainActivity.kt
+++ app/src/main/java/com/example/composestudy/MainActivity.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.AlertDialog
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,8 +16,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.example.composestudy.ui.theme.ComposeStudyTheme
 
 class MainActivity : ComponentActivity() {
@@ -47,26 +51,38 @@ class MainActivity : ComponentActivity() {
         }
 
         if (openDialog) {
-            AlertDialog(onDismissRequest = {
+            Dialog(onDismissRequest = {
                 openDialog = false
-            }, confirmButton = {
-                Button(onClick = {
-                    counter++
-                    openDialog = false
-                }) {
-                    Text(text = "더하기")
+            }) {
+                Surface {
+                    Column(
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text(text = "버튼을 클릭해주세요.\n * +1을 누르면 값이 증가됩니다.\n * -1을 누르면 값이 감소됩니다.")
+                        Row(
+                            modifier = Modifier.align(Alignment.End)
+                        ) {
+                            Button(onClick = {
+                                openDialog = false
+                            }) {
+                                Text(text = "취소")
+                            }
+                            Button(onClick = {
+                                openDialog = false
+                                counter++
+                            }) {
+                                Text(text = "+1")
+                            }
+                            Button(onClick = {
+                                openDialog = false
+                                counter--
+                            }) {
+                                Text(text = "-1")
+                            }
+                        }
+                    }
                 }
-            }, dismissButton = {
-                Button(onClick = {
-                    openDialog = false
-                }) {
-                    Text(text = "취소")
-                }
-            }, title = {
-                Text(text = "더하기")
-            }, text = {
-                Text(text = "더하기 버튼을 누르면 카운터가 증가합니다.\n 버튼을 눌러주세요.")
-            })
+            }
         }
     }
 
